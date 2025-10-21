3ba41832f9d3629e1eaa08fc0790ab2c517e5c4d
raghavkhandelwal-25 <raghav.work.khandewal@gmail.com>
2025-10-21 20:31:36 +0530

    sidebar added

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ let G1Entry = document.querySelector('#rpc-entry');
 let G2Entry = document.querySelector('#CF-entry');
 let G3Entry = document.querySelector('#TTT-entry');
 let G4Entry = document.querySelector('#entry7up');
+let menu=document.querySelector('#menu-btn');
+let sidebar=document.querySelector('.sidebar');
+let overlay=document.querySelector('.overlay');
+let menuBtns=document.querySelectorAll('.nav-link');
 // let saviorBtn=document.querySelector('#savior');
 
 let balance=parseInt(localStorage.getItem('balance')) || 1000; //saving balance in local storage
@@ -58,6 +62,25 @@ G4Entry.addEventListener('click', (e) => {
     window.location.href = '7up.html';
     }
 });
+
+menu.addEventListener('click',()=>{
+    sidebar.classList.toggle('open');
+    overlay.classList.toggle('open');
+});
+
+menuBtns.forEach((menuBtn)=>{
+    menuBtn.addEventListener('click',()=>{
+        sidebar.classList.remove('open');
+        overlay.classList.remove('open');
+    });
+});
+
+overlay.addEventListener('click',()=>{
+    sidebar.classList.remove('open');
+    overlay.classList.remove('open');
+});
+
+
 
 // saviorBtn.addEventListener('click',()=>{
 //     updateBalance(1000);
